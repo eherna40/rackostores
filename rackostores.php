f1dbad8c424f1a4a7be1bbcf3fabdb05aee1159c
Emilio <eherna40@gmail.com>
2021-01-08 17:54:31 +0100

    dev

--- rackostores.php
+++ rackostores.php
@@ -1,164 +1,144 @@
 <?php
+
 /**
-* 2007-2019 PrestaShop
-*
-* NOTICE OF LICENSE
-*
-* This source file is subject to the Academic Free License (AFL 3.0)
-* that is bundled with this package in the file LICENSE.txt.
-* It is also available through the world-wide-web at this URL:
-* http://opensource.org/licenses/afl-3.0.php
-* If you did not receive a copy of the license and are unable to
-* obtain it through the world-wide-web, please send an email
-* to license@prestashop.com so we can send you a copy immediately.
-*
-* DISCLAIMER
-*
-* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
-* versions in the future. If you wish to customize PrestaShop for your
-* needs please refer to http://www.prestashop.com for more information.
-*
-*  @author    PrestaShop SA <contact@prestashop.com>
-*  @copyright 2007-2019 PrestaShop SA
-*  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
-*  International Registered Trademark & Property of PrestaShop SA
-*/
+ * 2007-2019 PrestaShop
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License (AFL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/afl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to http://www.prestashop.com for more information.
+ *
+ *  @author    PrestaShop SA <contact@prestashop.com>
+ *  @copyright 2007-2019 PrestaShop SA
+ *  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+ *  International Registered Trademark & Property of PrestaShop SA
+ */
 
 if (!defined('_PS_VERSION_')) {
-    exit;
+  exit;
 }
 
 class Rackostores extends Module
 {
-    protected $config_form = false;
+  protected $config_form = false;
 
-    public function __construct()
-    {
-        $this->name = 'rackostores';
-        $this->tab = 'front_office_features';
-        $this->version = '1.0.0';
-        $this->author = 'LabelGrup';
-        $this->need_instance = 0;
-
-        /**
-         * Set $this->bootstrap to true if your module is compliant with bootstrap (PrestaShop 1.6)
-         */
-        $this->bootstrap = true;
-
-        parent::__construct();
-
-        $this->displayName = $this->l('Racko Stores/Tiendas');
-        $this->description = $this->l('Modulo de tiendas para rackosports');
-
-        $this->ps_versions_compliancy = array('min' => '1.6', 'max' => _PS_VERSION_);
-    }
+  public function __construct()
+  {
+    $this->name = 'rackostores';
+    $this->tab = 'front_office_features';
+    $this->version = '1.0.0';
+    $this->author = 'LabelGrup';
+    $this->need_instance = 0;
 
     /**
-     * Don't forget to create update methods if needed:
-     * http://doc.prestashop.com/display/PS16/Enabling+the+Auto-Update
+     * Set $this->bootstrap to true if your module is compliant with bootstrap (PrestaShop 1.6)
      */
-    public function install()
-    {
-        Configuration::updateValue('RACKO_STORES_LIVE_MODE', false);
+    $this->bootstrap = true;
 
-        return parent::install() &&
-            $this->registerHook('header');
+    parent::__construct();
+
+    $this->displayName = $this->l('Racko Stores/Tiendas');
+    $this->description = $this->l('Modulo de tiendas para rackosports');
+
+    $this->ps_versions_compliancy = array('min' => '1.6', 'max' => _PS_VERSION_);
+  }
+
+  /**
+   * Don't forget to create update methods if needed:
+   * http://doc.prestashop.com/display/PS16/Enabling+the+Auto-Update
+   */
+  public function install()
+  {
+    Configuration::updateValue('RACKO_STORES_LIVE_MODE', false);
+
+    return parent::install() &&
+      $this->registerHook('header');
+  }
+
+  public function uninstall()
+  {
+    Configuration::deleteByName('RACKO_STORES_LIVE_MODE');
+
+    return parent::uninstall();
+  }
+
+
+
+  /**
+   * Add the CSS & JavaScript files you want to be added on the FO.
+   */
+  public function hookHeader()
+  {
+    $this->context->controller->addJS($this->_path . '/views/js/front.js');
+    $this->context->controller->addCSS($this->_path . '/views/css/front.css');
+  }
+
+  public function hookDisplayFooter()
+  {
+    $idLang = Context::getContext()->language->id;
+    $showrooms = Store::getStores(1);
+    foreach ($showrooms as $key => $value) {
+      $showrooms[$key]['country'] = Country::getNameById($idLang, (int)$value['id_country']);
+      $showrooms[$key]['state'] = State::getNameById((int)$value['id_state']);
     }
 
-    public function uninstall()
-    {
-        Configuration::deleteByName('RACKO_STORES_LIVE_MODE');
-
-        return parent::uninstall();
-    }
-
-
-
-    /**
-     * Add the CSS & JavaScript files you want to be added on the FO.
-     */
-    public function hookHeader()
-    {
-        $this->context->controller->addJS($this->_path.'/views/js/front.js');
-        $this->context->controller->addCSS($this->_path.'/views/css/front.css');
-    }
-
-    public function hookDisplayFooter()
-    {
-      $idLang = Context::getContext()->language->id;
-      dump(new Stores::getSotres();
-      
-      $arreglo_spain = array();
-      $arreglo_wordlwide = array();
-      $stores = Db::getInstance()->executeS('SELECT DISTINCT a.id_customer , c.email, c.dato_extra1, s.name, c.website
+    $arreglo_spain = array();
+    $arreglo_wordlwide = array();
+    $stores = Db::getInstance()->executeS('SELECT DISTINCT a.id_customer , c.email, c.dato_extra1, s.name, c.website
                   FROM ' . _DB_PREFIX_ . 'address a
                   LEFT JOIN ps_customer c ON (c.id_customer = a.id_customer)
                   LEFT JOIN ps_state s ON (a.id_state = s.id_state)
                   WHERE c.active = 1 AND c.dato_extra2 = "1"
                   ORDER BY  s.name, a.city
-                  ');
+    ');
 
 
-                foreach ($stores as $key => $store) {
-  
-
-                $dato_extra = explode(';',$store['dato_extra1']);
-                $id_address = Address::getFirstCustomerAddressId($store['id_customer']);
-                $address = new Address($id_address);
-                $country = Country::getNameById($idLang, (int)$address->id_country);
-                $state = State::getNameById((int)$address->id_state);
-                $store['name_country'] = $country;
-                $store['id_country'] = $address->id_country;
-                $store['name'] = $address->alias;
-                $store['address1'] = $address->address1;
-                $store['city'] = $address->city;
-                $store['phone'] = $address->phone;
-                $store['phone_mobile'] = $address->phone_mobile;
-                $store['note'] = '';
-                $store['postcode'] = $address->postcode;
-                $store['state'] = $state;
-                $store['company'] = $address->company;
-                $store['web'] = $store['website'];
-                  if($address->id_country == "6"){
-                    $arreglo_spain[] = $store;
-                  }else{
-                    $arreglo_wordlwide[$country][] = $store;
-                  }
-              
-              }
+    foreach ($stores as $key => $store) {
 
 
-              $store = new Store(7);
-
-              $showroom = array(
-                'name' => $store->name[$this->context->language->id],
-                'address1' => $store->address1[$this->context->language->id],
-                'postcode' => $store->postcode,
-                'city' => $store->city,
-                'name_country' =>Country::getNameById($idLang, (int)$store->id_country),
-                'state' => State::getNameById((int)$store->id_state),
-                'note' => $store->note[$this->context->language->id],
-                'phone' => $store->phone,
-                'email' => $store->email,
-                'fax' => $store->fax,
-              );
-
-
-
-              // usort($arreglo_spain, function($a, $b) { return $a['state'] <=> $b['state']; });
-
-     
-      $this->context->smarty->assign(array(
-        'stores_spain' => $arreglo_spain,
-        'show' => $showroom,'stores_wordwide' => $arreglo_wordlwide, 
-        'lang' => $idLang));
-
-
-      return $this->display(__FILE__, 'views/templates/hook/stores.tpl');
+      $dato_extra = explode(';', $store['dato_extra1']);
+      $id_address = Address::getFirstCustomerAddressId($store['id_customer']);
+      $address = new Address($id_address);
+      $country = Country::getNameById($idLang, (int)$address->id_country);
+      $state = State::getNameById((int)$address->id_state);
+      $store['name_country'] = $country;
+      $store['id_country'] = $address->id_country;
+      $store['name'] = $address->alias;
+      $store['address1'] = $address->address1;
+      $store['city'] = $address->city;
+      $store['phone'] = $address->phone;
+      $store['phone_mobile'] = $address->phone_mobile;
+      $store['note'] = '';
+      $store['postcode'] = $address->postcode;
+      $store['state'] = $state;
+      $store['company'] = $address->company;
+      $store['web'] = $store['website'];
+      if ($address->id_country == "6") {
+        $arreglo_spain[] = $store;
+      } else {
+        $arreglo_wordlwide[$country][] = $store;
+      }
     }
 
-    public function hookRackStores(){
+    $this->context->smarty->assign(array(
+      'stores_spain' => $arreglo_spain,
+      'showrooms' => $showrooms,
+      'stores_wordwide' => $arreglo_wordlwide,
+      'lang' => $idLang
+    ));
 
-    }
 
+    return $this->display(__FILE__, 'views/templates/hook/stores.tpl');
+  }
 }
--- views/templates/hook/stores.tpl
+++ views/templates/hook/stores.tpl
@@ -1,33 +1,41 @@
+{if $page.meta['title'] != 'Racko e-partners'}
+    <div class="row">
+        <div class="col-12 custom-shops-title text-center" style='border-bottom: 0 !important'>
+            {l s='RACKO' mod='racko_stores'}</div>
 
-{if $page.meta['title'] != 'Racko e-partners'} 
-<div class="row">
-    <div class="col-12 custom-shops-title text-center" style='border-bottom: 0 !important'>{l s='RACKO' mod='racko_stores'}</div>
+        <div class="col-12 custom-shops-title pb-4 text-center  d-md-none"
+            style="margin: 0 !important; line-height: 1;     border-bottom: 0px solid #eee;">
+            {l s='SHOWROOM' mod='racko_stores'}</div>
+        <div class="col-12 custom-shops-title pb-4 text-center  d-md-none"
+            style="margin: 0 !important; line-height: 1;    border-bottom: 0px solid #eee;">{l s='&' mod='racko_stores'}
+        </div>
+        <div class="col-12 custom-shops-title pb-4 text-center d-block  d-md-none"
+            style="margin-top: 0 !important; line-height: 1;    border-bottom: 0px solid #eee;">
+            {l s='STORE' mod='racko_stores'}</div>
 
-    <div class="col-12 custom-shops-title pb-4 text-center  d-md-none" style="margin: 0 !important; line-height: 1;     border-bottom: 0px solid #eee;">{l s='SHOWROOM' mod='racko_stores'}</div>
-        <div class="col-12 custom-shops-title pb-4 text-center  d-md-none" style="margin: 0 !important; line-height: 1;    border-bottom: 0px solid #eee;">{l s='&' mod='racko_stores'}</div>
-            <div class="col-12 custom-shops-title pb-4 text-center d-block  d-md-none" style="margin-top: 0 !important; line-height: 1;    border-bottom: 0px solid #eee;">{l s='STORE' mod='racko_stores'}</div>
 
+        <div class="col-12 custom-shops-title pb-4 text-center d-none d-md-block"
+            style="margin-top: 0 !important; line-height: 1">{l s='SHOWROOM & STORE' mod='racko_stores'}</div>
+        {foreach from=$showrooms item=$show key=key}
+            <div class="col-12 col-md-4 text-center">
+                <div class="custom-shop-card">
+                    <div class="custom-shops-name">{$show.name}</div>
+                    <div class="custom-shops-name">{$show.state} ({$show.country})</div>
+                    <div class="">{$show.address1}</div>
+                    <div class="">{$show.postcode}, {$show.city}</div>
+                    <div class="custom-shops-email"><a href="tel:{$show.phone}">{$show.phone}</a>{if $show.fax} /
+                        <a href="tel:{$show.fax}">{$show.fax}</a>{/if}
+                    </div>
+                    <div class="custom-shops-email"><a href="mailto:{$show.email}">{$show.email}</a></div>
+                    <div class="custom-shops-web.note" style="color: balck"><a>{$show.note}</a></div>
+                </div>
+            </div>
+        {/foreach}
 
-    <div class="col-12 custom-shops-title pb-4 text-center d-none d-md-block" style="margin-top: 0 !important; line-height: 1">{l s='SHOWROOM & STORE' mod='racko_stores'}</div>
-	<div class="col-12 col-md-4 text-center">
-		<div class="custom-shop-card">
-        	<div class="custom-shops-name">{$show.name}</div>
-        	<div class="custom-shops-name">{$show.state} ({$show.name_country})</div>
-			<div class="">{$show.address1}</div>
-			<div class="">{$show.postcode}, {$show.city}</div>
-<div class="custom-shops-email"><a href="tel:{$show.phone}">{$show.phone}</a>{if $show.fax} / <a href="tel:{$show.fax}">{$show.fax}</a>{/if}</div>
-			<div class="custom-shops-email"><a  href="mailto:{$show.email}">{$show.email}</a></div>
-			<div class="custom-shops-web.note" style="color: balck"><a>{$show.note}</a></div>
+        <div class="col-12 col-md-4 text-center">
+            <img src="{$urls.img_url}/canguro_racko.jpg" style="max-width: 200px;" />
+        </div>
     </div>
-    
-
-    
-    </div>
-
-      <div class="col-12 col-md-4 text-center">
-        <img src="{$urls.img_url}/canguro_racko.jpg"  style="max-width: 200px;" />
-      </div>
-</div>
 
 {/if}
 
@@ -35,76 +43,79 @@
 
 
 <div class="stores-container">
-  <div class="row">
-    <div class="col-12 custom-shops-title pb-4 text-center">{l s='España' mod='racko_stores'}</div>
-    <div class="col-12 stores-inner d-flex flex-wrap p-0">
-      {foreach key=key item=store from=$stores_spain}
-      <div class="col-12 col-md-4">
-      <div class="custom-shop-card">
-	  	{if $page.meta['title'] != 'Racko e-partners'} 
-			<div class="custom-shops-name" style="font-weight: normal">{$store.state}{if $store.city != $store.state } - {$store.city}{/if}</div>
-		{/if}
-        <div class="custom-shops-name">{$store.name}</div>
-        {if $store.company != '-'}
-        <div class="custom-shops-name">{$store.company}</div>
-{/if}
-{if $page.meta['title'] != 'Racko e-partners'} 
-        <div class="">{$store.address1}</div>
-        <div class="">{$store.postcode}, {$store.city}</div>
-        <div class="">{$store.state}</div>
-{/if}
-<div class="d-flex align-itemsc
-">
-{if $store.phone}
-<div class="custom-shops-email"><a href="tel:{$store.phone_mobile}">{$store.phone}</a> </div> {if $store.phone_mobile}
-<span class="mx-2">/</span> {/if}
-{/if}
-{if $store.phone_mobile}
-<div class="custom-shops-email">
-    <a href="tel:{$store.phone_mobile}">{$store.phone_mobile}</a></div>
+    <div class="row">
+        <div class="col-12 custom-shops-title pb-4 text-center">{l s='España' mod='racko_stores'}</div>
+        <div class="col-12 stores-inner d-flex flex-wrap p-0">
+            {foreach key=key item=store from=$stores_spain}
+                <div class="col-12 col-md-4">
+                    <div class="custom-shop-card">
+                        {if $page.meta['title'] != 'Racko e-partners'}
+                            <div class="custom-shops-name" style="font-weight: normal">
+                                {$store.state}{if $store.city != $store.state } - {$store.city}{/if}</div>
+                        {/if}
+                        <div class="custom-shops-name">{$store.name}</div>
+                        {if $store.company != '-'}
+                            <div class="custom-shops-name">{$store.company}</div>
+                        {/if}
+                        {if $page.meta['title'] != 'Racko e-partners'}
+                            <div class="">{$store.address1}</div>
+                            <div class="">{$store.postcode}, {$store.city}</div>
+                            <div class="">{$store.state}</div>
+                        {/if}
+                        <div class="d-flex align-itemsc
+        ">
+                            {if $store.phone}
+                                <div class="custom-shops-email"><a href="tel:{$store.phone_mobile}">{$store.phone}</a> </div>
+                                {if $store.phone_mobile}
+                                <span class="mx-2">/</span> {/if}
+                            {/if}
+                            {if $store.phone_mobile}
+                                <div class="custom-shops-email">
+                                    <a href="tel:{$store.phone_mobile}">{$store.phone_mobile}</a>
+                                </div>
 
-{/if}
-</div>
-        <div class="custom-shops-web"><a href="{$store.web}">{$store.web}</a></div>
-			<div class="custom-shops-web"><a href="mailto:{$store.email}">{$store.email}</a></div>
+                            {/if}
+                        </div>
+                        <div class="custom-shops-web"><a href="{$store.web}">{$store.web}</a></div>
+                        <div class="custom-shops-web"><a href="mailto:{$store.email}">{$store.email}</a></div>
 
-      </div>
-    </div>
-      {/foreach}
-    </div>
+                    </div>
+                </div>
+            {/foreach}
+        </div>
 
-
-  </div>
-      <div class="line"></div>
-{foreach key=key item=store_world from=$stores_wordwide}
-  <div class="row">
-    <div class="col-12 custom-shops-title pb-4 text-center">{$key}</div>
-    <div class="col-12 stores-inner d-flex flex-wrap">
-
-
-{foreach key=key item=store from=$store_world}
-<div class="col-12 col-md-4">
-
-      <div class="custom-shop-card">
-        <div class="custom-shops-name">{$store.name}</div>
-        <div class="custom-shops-name">{$store.state}</div>
-        <div class="">{$store.address1}</div>
-        <div class="">{$store.postcode}, {$store.city}</div>
-
-
-        <div class="custom-shops-email"><a>{$store.phone}</a></div>
-        <div class="custom-shops-email"><a>{$store.email}</a></div>
-        <div class="custom-shops-web"><a>{$store.note}</a></div>
-      </div>
-    </div>
-
-    {/foreach}
 
     </div>
+    <div class="line"></div>
+    {foreach key=key item=store_world from=$stores_wordwide}
+        <div class="row">
+            <div class="col-12 custom-shops-title pb-4 text-center">{$key}</div>
+            <div class="col-12 stores-inner d-flex flex-wrap">
 
 
-  </div>
-  <div class="line"></div>
+                {foreach key=key item=store from=$store_world}
+                    <div class="col-12 col-md-4">
+
+                        <div class="custom-shop-card">
+                            <div class="custom-shops-name">{$store.name}</div>
+                            <div class="custom-shops-name">{$store.state}</div>
+                            <div class="">{$store.address1}</div>
+                            <div class="">{$store.postcode}, {$store.city}</div>
+
+
+                            <div class="custom-shops-email"><a>{$store.phone}</a></div>
+                            <div class="custom-shops-email"><a>{$store.email}</a></div>
+                            <div class="custom-shops-web"><a>{$store.note}</a></div>
+                        </div>
+                    </div>
+
+                {/foreach}
+
+            </div>
+
+
+        </div>
+        <div class="line"></div>
     {/foreach}
 
 </div>
